fd8c9c6844f49373f0d6228e6b739fa1a2753968
PH4N7OMx <averkostya@gmail.com>
2026-09-04 18:26:22 +0200

    feat(ayu): integrate liquid glass into chat top bar

--- Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
+++ Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "history/view/history_view_top_bar_widget.h"
 
+#include "ayu/ui/ayu_liquid_glass.h"
 #include "history/history.h"
 #include "history/view/history_view_send_action.h"
 #include "boxes/add_contact_box.h"
@@ -545,7 +546,17 @@ void TopBarWidget::paintEvent(QPaintEvent *e) {
 		: -st::topBarHeight;
 	const auto slidingTop = std::max(selectedButtonsTop, searchFieldTop);
 
-	p.fillRect(QRect(0, 0, width(), st::topBarHeight), st::topBarBg);
+	if (AyuLiquidGlass::isEnabled(LiquidGlassMode::ChatBars)) {
+		AyuLiquidGlass::paintGlass(
+			p,
+			QRect(0, 0, width(), st::topBarHeight),
+			st::topBarBg->c,
+			this,
+			false,
+			true);
+	} else {
+		p.fillRect(QRect(0, 0, width(), st::topBarHeight), st::topBarBg);
+	}
 	if (slidingTop < 0) {
 		p.translate(0, slidingTop + st::topBarHeight);
 		paintTopBar(p);
